58c9d61ba50bba696d28734fcb5aa760ba75c4c2
Max <max@staneker.com>
2026-03-11 22:38:22 +0100

    Update docs assets and site styles
    Rebuild docs assets with updated hashed filenames and theme tweaks. Several compiled CSS/JS assets in docs/assets were replaced or renamed (new App and LandingPage CSS, renamed JS bundles and BrandMark), and obsolete hashed files removed. Also update docs/index.html and source styles (src/App.css, src/landing/LandingPage.css) to reflect styling adjustments included in the rebuild.

--- docs/assets/index-CK0t79QZ.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-D0DDqvVO.js";async function r(e={}){return typeof e=="object"&&Object.freeze(e),await n("plugin:dialog|open",{options:e})}export{r as open};
---
+++ docs/assets/index-wj3RPAPn.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-BM04-NAj.js";async function r(e={}){return typeof e=="object"&&Object.freeze(e),await n("plugin:dialog|open",{options:e})}export{r as open};
